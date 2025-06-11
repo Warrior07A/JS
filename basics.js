1c66920621ad13fdf5d1b9dabd02089e5c835133
Warrior07A <akshatmittalofficial@gmail.com>
2025-06-11 19:54:28 +0000

    maps filter and more

--- basics.js
+++ basics.js
@@ -28,3 +28,51 @@ function fodo(arr,target){
 }
 arr=[1,2,3,4,5];
 fodo(arr,5);
+
+//forEach function
+
+var arr=[1,2,3,4];
+arr.forEach(function(val){              //access every elemnt of an array one by one.
+    console.log(val+"hello");           //1hello ...2hello...3hello...4hello...
+})
+
+
+//map        (works on arrays) and gives a new array
+const input=[1,2,3,4];
+function transform(i){
+    return (i*2);
+}
+const ans=input.map(transform);
+console.log(ans);                                   //output: [2,4,6,8]
+
+
+//filter
+//It filters out elements from an array based on some logic
+
+const arr2=[1,2,3,4];
+function filterLog(n){
+    if(n%2==0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+const ans2=array.filter(filterLog);              //[2,4]
+
+
+//find
+//finds the element at the first occurence
+const arr3=[1,2,3,4];
+var ans4=arr3.find(function(val){
+    if (val==2) return val;
+})
+console.log(ans4);
+
+
+//indexOf
+const arr4=[1,2,3,4];
+arr4.indexOf(element)             //directly put whatver u have to find; let it be a no or value;directly replce with element
+                                   //retuns(-1) if not found or returns first occrrence
+
+                                   
