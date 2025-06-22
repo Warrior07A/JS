a4ad6f2d755fb75577646b0a22838cd07f19d9d7
warrior07A <akshatmittalofficial@gmail.com>
2025-06-22 19:04:38 +0530

    HTTP

--- 12.HTTP+MIDDL.js
+++ 12.HTTP+MIDDL.js
@@ -8,23 +8,29 @@ let noofreq=0;
 
 
 function requestIncreaser(req,res,next){
-    noofreq+=1;
+    console.log("Method is"+ req.method);                          //logs the method used
+    console.log("URL IS "+req.hostname);                                //logs the url
+    console.log(new Date());                                        //logs th date
+    console.log("route is "+ req.url);
     next();
 }
 
-app.get("/multiply",requestIncreaser,function(req,res){           //there need to be a next in fn to continue otherwise wont work
+app.use(requestIncreaser);
+
+app.get("/multiply",function(req,res){           //there need to be a next in fn to continue otherwise wont work
     const a=req.query.a
     const b=req.query.b
+    noofreq+=1;
     res.json({                                                 //you can only send one request to theserver=> one res.json
         ans:a*b,                        //include everything in there! by commas
-        noofreq
+        noofreq,
     })
-    
 })
 
 app.get("/sum",requestIncreaser,function(req,res){
     const a=parseInt(req.query.a);          ////problem comes in sum and subtract since it takes query input as
-    const b=parseInt(req.query.b)         // "1" + "2"  yes as strings    
+    const b=parseInt(req.query.b) 
+    noofreq+=1;                                             // "1" + "2"  yes as strings    
     res.json({
         ans:a+b,
         noofreq
@@ -36,10 +42,4 @@ app.listen(3000);
 
 
 
-//CROSS ORIGIN RESOURCE SHARING
 
-//if you'r sending a fetch req to the server and the server is returning you with imp data over to you
-//that's a vulnerability that needs to be fixed
-//eg google.com Front end sending fetch data req to hdfcnetbanking BE 
-// Normally node.js do not allow "CORS" and others also block any 3rd party request thereby closing the vulner port!
-//link="https://petal-estimate-4e9.notion.site/cors-Cross-origin-resource-sharing-e629aed258c04a669cbe2de1f13a9ac3"
