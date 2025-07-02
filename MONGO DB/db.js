1902d3e8d8020309bd75b88bc40dc8a9fb83a38e
warrior07A <akshatmittalofficial@gmail.com>
2025-07-03 02:58:12 +0530

    schema and created DB

--- MONGO DB/db.js
+++ MONGO DB/db.js
@@ -1,4 +1,26 @@
 const mongoose=require("mongoose");
 
-const Schema=moongoose.Schema;
+const Schema=mongoose.Schema;                                                      //mongoose exports a class called schema
 const ObjectId=mongoose.ObjectId;
+
+
+const User=new Schema({
+    email:{type:String,unique:true},
+    name:String,
+    password:String
+})
+
+const Todo=new Schema({
+    title:String,
+    done:Boolean,
+    userID:ObjectId
+})
+
+const Usermodel=mongoose.model("users",User);                                    //creates a new database of the user in users db with schema User
+const Todomodel=mongoose.model("todo",Todo);                                     //model does that work to create
+
+
+module.exports={
+    Usermodel:Usermodel,                                                            //helps to import these two to index.js
+    Todomodel:Todomodel
+}
--- MONGO DB/index.js
+++ MONGO DB/index.js
@@ -1,23 +1,83 @@
 const  express=require("express");
-
 const app=express();
+const {Usermodel,Todomodel}=require("./db");                                            //exported these two from db.js  //in short linked mongodb
+const mongoose=require("mongoose");
+const jwt=require("jsonwebtoken");
+const JWT_SECRET="ITSSECRET";
+
+mongoose.connect("mongodb+srv://Warrior07A:5bglU72b85qpC15G@cluster0.ru0uizx.mongodb.net/todo")   //after cluster name /todo (name of db )if not creates a new one
 app.use(express.json());
 
 
-app.post("/signup",(req,res)=>{
+app.post("/signup",async function(req,res){                                             //since creation of data to mongodb server takes some time
+    const email=req.body.email;
+    const password=req.body.password;
+    const name=req.body.name;
+
+    await Usermodel.create({
+        email:email,
+        name:name,
+        password:password
+        
+    })
+    res.json({
+        msg:"you have been signed up"
+    })
 
 })
 
-app.post("/signin",(req,res)=>{
+app.post("/signin",async function(req,res){
+    const email=req.body.email;
+    const password=req.body.password;
 
+    const user=await Usermodel.findOne({
+        email:email,
+        password:password  
+ })
+
+ console.log(user)
+
+    if (user){
+        const token=jwt.sign({
+            id:user._id.toString()                                              //_id is of objectid class that is not defined when decoded hence need to convert it to string for use
+    },JWT_SECRET);
+        res.json({
+            token:token
+        })
+    }else{
+        res.status(403).json({
+             msg:"INvalid credentials"
+        })
+    }
 })
 
-app.post("/todo",(req,res)=>{
+app.post("/todo",auth,(req,res)=>{
+    const userid=req.userID;
+    res.json({
+        userid:userid
+    })
 
 })
 
 app.post("/todos",(req,res)=>{
+     const userid=req.userID;
+      res.json({
+        userid:userid
+    })
 
 })
+
+async function auth(req,res,next){
+    const token=req.headers.token;
+    const decodedtoken=jwt.verify(token,JWT_SECRET);
+    if (decodedtoken){
+        req.userID=decodedtoken.id
+        next()
+    }else{
+        res.status(403).json({
+            msg:"INCORRECT CREDENTIALS"
+        })
+    }
+}
 
 app.listen(3000);
